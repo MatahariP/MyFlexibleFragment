ed8ae47c57e0fa6271d85e1908a343af7228e3b5
Matahari Pradipta <mataharipradipta02@gmail.com>
2023-10-10 15:20:46 +0700

    tes

--- app/src/main/java/com/example/myflexiblefragment/MainActivity.java
+++ app/src/main/java/com/example/myflexiblefragment/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myflexiblefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        HomeFragment mHomeFragment = new HomeFragment();
+        mFragmentTransaction.add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName());
+        Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment.class.getSimpleName());
+        mFragmentTransaction.commit();
+
     }
 }
